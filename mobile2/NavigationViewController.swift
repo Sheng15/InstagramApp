82254328cea5609afcf90b63990d3c1d46ea694a
GarrettZhao <43398251+GarrettZhao@users.noreply.github.com>
2018-10-11 18:45:01 +1100

    add page
    discover and navigation view controller build

---
+++ mobile2/NavigationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NavigationViewController.swift
+//  mobile2
+//
+//  Created by 赵子健 on 2018/10/11.
+//  Copyright © 2018年 LudwiG. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+    
+    //default func
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // color of title at the top in nav controller
+        self.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        
+        // color of buttons in nav controller
+        self.navigationBar.tintColor = .white
+        
+        // color of background of nav controller
+        self.navigationBar.barTintColor = UIColor(red: 18.0 / 255.0, green: 86.0 / 255.0, blue: 136.0 / 255.0, alpha: 1)
+        
+        // disable translucent
+        self.navigationBar.isTranslucent = false
+    }
+    
+    // white status bar function
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+}
